4cd5fd99bcb68cd62681de9618daa36023d50c24
lucas <gepeoverload@gmail.com>
2022-06-28 12:21:19 +0200

    added 2 different files to show in case the game is still going or it is dead(at least on of the players lost)

---
+++ game-alive.php
@@ -0,0 +1,6 @@
+</div>     
+<form action="index.php" method="post">
+<button name="player-action-hit" type="submit">HIT</button>
+<button name="player-action-stand" type="submit">STAND</button>
+<button name="player-action-surrender" type="submit">SURRENDER</button>
+</form>
---
+++ game-dead.php
@@ -0,0 +1,4 @@
+</div>     
+<form action="index.php" method="post">
+<button name="player-action-restart" type="submit">RESTART</button>
+</form>
